6cf7bfdfa7b69c33a37d3b4f00c93fd7a94f882e
Daniel Henrique da Costa Ferreira <danielcosta@tjrj.jus.br>
2021-11-11 10:15:40 -0300

    arquivos do curso

---
+++ javascript/calculos.js
@@ -0,0 +1,18 @@
+export const somar = (a, b) => {
+    return (a + b);
+}
+
+export const subtrair = (a, b) => {
+    return (a - b);
+}
+
+//exporta funções para outros arquivos js
+// module.exports = {
+//     somar,
+//     subtrair
+// }
+
+// export default {
+//     somar,
+//     subtrair
+// }
---
+++ javascript/chamarFuncaoCalculos.js
@@ -0,0 +1,15 @@
+// const {somar, subtrair}  = require("./calculos"); 
+// ou 
+// const calculos =  require("./calculos"); 
+// ou usando import
+
+import {somar, subtrair} from './calculos.js';
+
+// const result =  somar(5, 2);
+// const result2 = subtrair(5, 2);
+
+const result3 = somar(2, 2);
+
+// console.log(result);
+// console.log(result2);
+console.log(result3);
